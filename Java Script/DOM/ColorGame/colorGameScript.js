cd8e6b4192748bcf4f64786a4cf11fcbae32a1b1
Ekaterina Stepanova <katia.yurevna@gmail.com>
2018-09-10 20:46:07 -0700

    Final
    Everything works

--- Java Script/DOM/ColorGame/colorGameScript.js
+++ Java Script/DOM/ColorGame/colorGameScript.js
@@ -1,6 +1,6 @@
 var squareDisplayed = document.querySelectorAll("#test");
 var messageDisplay = document.querySelector("#newMessage");
-var resetButton = document.querySelector("button");
+var newColors = document.querySelector("button");
 var easyBtn = document.getElementById("easy");
 var hardBtn = document.getElementById("hard");
 var headerRGB = document.querySelector("#headerRGB");
@@ -10,21 +10,22 @@ var pickedColor;
 var defaultSquareDisplayed = 6; 
 var easyMode = 3;
 var hardMode = 6;
+var gameMode = 0;
 
-drawSquares(hardMode);
-generateRandomColor(hardMode);
-changeColor(hardMode);
-clickedSquare(hardMode);
+drawSquares(gameMode);
+generateRandomColor(gameMode);
+changeColor(gameMode);
+clickedSquare(gameMode);
 
-function drawSquares(hardMode){ 
-    for (var i = 0; i < hardMode; i++){
+function drawSquares(gameMode){ 
+    for (var i = 0; i < gameMode; i++){
         var node = document.createElement("div");
         node.setAttribute("class", "square");
         var element = document.getElementById("container");
         element.appendChild(node);
         var square = document.querySelectorAll(".square");
     }
-    return hardMode;
+    return gameMode;
 }
 
 function randomColor(){
@@ -34,9 +35,9 @@ function randomColor(){
         return "rgb(" + x  + "," + " " + y +  "," + " " + z + ")";
 }
 
-function generateRandomColor(hardMode){
+function generateRandomColor(gameMode){
     colorArray=[];
-    for ( var i = 0; i<hardMode; i++){
+    for ( var i = 0; i<gameMode; i++){
         colorArray.push(randomColor());      
 }
 return colorArray;
@@ -50,18 +51,18 @@ function pickColor(){
     return pickedColor;
 }
 
-function changeColor(hardMode){
+function changeColor(gameMode){
     var square = document.querySelectorAll(".square");
-    for (var i = 0; i< hardMode; i++){
+    for (var i = 0; i< gameMode; i++){
         square[i].style.background = colorArray[i];
     }
-    return hardMode;
+    return gameMode;
 }
 
-function clickedSquare(hardMode){
+function clickedSquare(gameMode){
     var clickedColor;
     var square = document.querySelectorAll(".square");
-    for ( var i = 0; i< hardMode; i++){
+    for ( var i = 0; i< gameMode; i++){
         square[i].addEventListener("click", function(){
            clickedColor = this.style.backgroundColor; 
            console.log("Clicked= ", clickedColor);
@@ -76,96 +77,47 @@ function clickedSquare(hardMode){
         })
     }
 }
- /*    
-function addNewClass(squareForHardMode){
-    for (var i = 0; i<squareForHardMode; i++){
-        squareDisplayed[i].classList.add("square");
-        }
-    return squareForHardMode;
-}
-
-/*function removeClass(squareForEasyMode){
-    var squareDisplayed.length = squareForEasyMode;
-    for (var i = 0; i<squareForEasyMode; i++){
-        squareDisplayed[i].classList.remove("square");
-        }
-    console.log (squareDisplayed.length);
-    return squareForEasyMode;
-}
-
-
-resetButton.addEventListener("click",function(){
-    squareDisplayed = document.querySelectorAll("#test");
-    var resultFunction1 = generateRandomColor();
-    var pickedColor = pickColor();
-    var temp = squareDisplayed;
+ 
+newColors.addEventListener("click",function(){
+    squareDisplayed = document.querySelectorAll(".square");
+    var colorArray = generateRandomColor(gameMode);
+    console.log(colorArray);
+    pickColor();
     messageDisplay.textContent =""; 
-    
-    for(var i = 0; i <temp.length; i++) {
-		temp[i].style.background = colorArray[i];
-	}
-    return resultFunction1;
+    for(var i = 0; i < squareDisplayed.length; i++) {
+		squareDisplayed[i].style.background = colorArray[i];
+    }
 });
-*/
+
 easyBtn.addEventListener("click", function(){
     var parentContainer = document.querySelector("#container");
     while (parentContainer.firstChild){
         parentContainer.removeChild(parentContainer.firstChild);
     }
-    hardMode = 3;
-    drawSquares(hardMode);
-    generateRandomColor(hardMode);
-    changeColor(hardMode);
-    clickedSquare(hardMode);
+    gameMode = easyMode;
+    drawSquares(gameMode);
+    generateRandomColor(gameMode);
+    pickColor();
+    changeColor(gameMode);
+    clickedSquare(gameMode);
 })
 
-    //square.parentNode.removeChild(square)
-    //hardMode = easyMode;
-    /*
-    drawSquares(hardMode);
-    //generateRandomColor(hardMode);
-    //changeColor(hardMode);
-    //clickedSquare(hardMode);
-    
-    })*/
- /*   
-    })
-    //return newVariable;
-    //easyBtn.classList.add("btnEasy");
-    //hardBtn.classList.remove("btnHard");
-    //drawSquares();
-    //squareDisplayed.length = squareForEasyMode;
-    //return squareDisplayed;
-*/
 
 hardBtn.addEventListener("click", function(){
     var parentContainer = document.querySelector("#container");
     while (parentContainer.firstChild){
         parentContainer.removeChild(parentContainer.firstChild);
     }
-    hardMode = 6;
-    drawSquares(hardMode);
-    generateRandomColor(hardMode);
-    changeColor(hardMode);
-    clickedSquare(hardMode);
+    gameMode = hardMode;
+    drawSquares(gameMode);
+    generateRandomColor(gameMode);
+    pickColor();
+    changeColor(gameMode);
+    clickedSquare(gameMode);
     })
-/*
-function drawSquares(){ 
-    for (var i = 0; i<squareDisplayed.length; i++){
-        squareDisplayed[i].classList.add("square");
-    }
-    return squareDisplayed;
-}
 
 
 
-// default loaded page
-drawSquares();
-generateRandomColor();
-pickColor()
-changeColor();
-clickedSquare();
 
-*/
 
 
