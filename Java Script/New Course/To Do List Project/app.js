8b6e118e722df0e9fa604df81b6097ffb87e731c
Ekaterina Stepanova <katia.yurevna@gmail.com>
2018-09-21 15:39:08 -0700

    To do list project
    Final version

--- Java Script/New Course/To Do List Project/app.js
+++ Java Script/New Course/To Do List Project/app.js
@@ -1,24 +1,82 @@
 
-var form = document.querySelector("#task-form");
-var taskInput = document.querySelector('#task');
+const form = document.querySelector("#task-form");
+const taskInput = document.querySelector('#task');
+var taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
+var clearBtn = document.querySelector('.clear-tasks');
+
+loadEventListener();
 
 
-form.addEventListener("submit",
-function(event){
-    if (taskInput.value==''){
-        alert("Add a task");
+function loadEventListener(){
+    //Add Task
+    form.addEventListener("submit",addItem);
+    //Delete Task
+    document.body.addEventListener('click', delItem);
+    //Clear Tasks
+    clearBtn.addEventListener('click',ClearTasks);
+    //Filter Tasks
+    filter.addEventListener('keyup',FilterTasks);
+}
+
+function addItem(event){
+        if (taskInput.value==''){
+            alert("Add a task");
+            event.preventDefault();
+            return;           
+        }
+        
+        //Create li element
+        const li = document.createElement("li");
+        //Add class
+        li.className = 'collection-item';
+        //Create text node and append to li
+        const textNode = document.createTextNode(taskInput.value);
+        ////Append textnode to li
+        li.appendChild(textNode);
+        //Create a link 
+        const link = document.createElement('a');
+        //Add class
+        link.className = 'delete-item secondary-content';
+        //Add icon html
+        link.innerHTML = '<i class = "fa fa-remove"></i>';
+        //Append the link to li
+        li.appendChild(link);
+        // Append li to ul
+        taskList.appendChild(li);
+        //Clear input
+        form.reset();
+
+        event.preventDefault();
+
+}
+
+function delItem(e){
+    if (e.target.parentElement.className === 'delete-item secondary-content'){
+        if(confirm("Are you sure?")){
+        e.target.parentElement.parentElement.remove();
+        }
     }
-    //Create li element
-    const li = document.createElement("li");
-    //Add class
-    li.className = 'collection-item';
-    //Create text node and append to li
-    var textNode = document.createTextNode(taskInput.value);
-    li.appendChild(textNode);
-    document.querySelector(".collection").appendChild(li);
+}
 
+function ClearTasks(){
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+        //taskList.innerHTML = ' ';
+    }
+    console.log("deleted");
+}
 
-    form.reset();
-    event.preventDefault();
-});
-
+function FilterTasks(e){
+    var text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1){
+            console.log("great");
+            task.style.display = "block";
+        }
+        else{
+            task.style.display ='none';
+        }  
+    });
+}
