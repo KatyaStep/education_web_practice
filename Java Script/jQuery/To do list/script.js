b9b7290ef37298890af206c5a3f97baed70945de
Ekaterina Stepanova <katia.yurevna@gmail.com>
2018-11-06 12:50:52 -0800

    To-Do-List Project (Final Part)
    There are 3 files:
    - todolist.html
    - style.css
    - script.js
    
    To do list functionality:
    -add new tasks
    -mark finished tasks
    -delete tasks from the list

--- Java Script/jQuery/To do list/script.js
+++ Java Script/jQuery/To do list/script.js
@@ -1,20 +1,36 @@
 
-console.log("Connected");
-
-$('li').on("mouseenter", function(){
-    $(this).append("<a href = # class = 'link' ><i class='fas fa-trash-alt fa-sm'> </i> </a>")
-    let item = $(this);
-    console.log(item);
-    $('.link').on("click",function(item){
-        $(this).parent().remove();
+// Show trash-can and delete functionality
+$('ul').on("mouseenter", "li", function(){
+    //$(this).append("<a href = # class = 'link' ><i class='fas fa-trash-alt fa-sm'> </i> </a>")
+     $(this).children('span').toggleClass("trash-can");
+    $('span').on("click", function(event){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        })
+       event.stopPropagation(); //Prevents the event from bubbling up the DOM tree
     })
 })
+// Hide a trash-can
+$('ul').on("mouseleave","li", function(){
+    $(this).children('span').toggleClass("trash-can");
+})
 
-
-$('li').on("mouseleave", function(){
-    $(".fa-trash-alt").remove();
-    $(".link").remove();
+//Check Off Specific  Todo By Clicking
+$('ul').on("click", "li", function(){
+    $(this).toggleClass("completed");
+})
     
+// Add a new task
+$("input[type='text']").on('keypress', function(event){
+    if (event.which == '13'){
+        let todoText = $(this).val();
+        if (todoText == ""){
+            alert("Input field cannot  be empty");
+        }else {
+            $(".tasks").append(`<li><span class = 'fas fa-trash-alt fa-sm'></span>${todoText}</li>`);
+            $(this).val("");
+        }
+    }
 })
 
 
